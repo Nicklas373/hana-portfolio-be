2675ab9b3dda78f7b2616e8621d8cd81d83a14a7
Dicky Herlambang (花） <herlambangdicky5@gmail.com>
2026-09-14 19:56:05 +0700

    zod: Loosen a bit validation for message input on contactMenu

--- src/lib/zod/schema.ts
+++ src/lib/zod/schema.ts
@@ -3,5 +3,6 @@ import * as z from "zod";
 export const charWithDigitSchema = (digits: number) =>
   z
     .string()
-    .regex(/^[a-zA-Z0-9 ]+$/, "Input must contain only alphanumeric characters")
+    .trim()
+    .min(1, "Message is required")
     .max(digits, `Input must not exceed ${digits} characters`);
